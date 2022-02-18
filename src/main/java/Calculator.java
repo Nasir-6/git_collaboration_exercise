fbdc57eb1858091fe11d11c8e9c83d60c1b6fac1
Nasir <nasir-islam@hotmail.com>
2022-02-18 12:14:57 +0000

    fix fix branch

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
 
-    public static double multiply(double a, double b ){
+    public static int multiply(int a, int b ){
         return a*b;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println(Calculator.multiply(2,3));
+        System.out.println("testing fix branch");
     }
 }
